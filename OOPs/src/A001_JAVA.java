d86f7ff6b2c42ce94c38efdcbf9d8dc9fd387902
Aryan <aryangithub2017@gmail.com>
2025-02-09 14:42:06 +0530

    push
    initial commit

---
+++ OOPs/src/A001_JAVA.java
@@ -0,0 +1,35 @@
+/*
+What is JAVA?
+Java is a programming language that allow you to play online games, chat with people
+around the world and quickly calculate tip at a meal.
+
+It's an object-oriented language
+
+JAVA can be used to create web application, android apps and much more.
+
+It's a great foundational language as it is very similar to other coding languages,
+and it is designed to be portable and workable  as many platform as possible.
+
+IDE stands for integrated development environment.
+it's a software that we can use to write code.
+it also contains debugger to find out any error.
+
+JDK - Java Development Kit.
+The JDK is a fully featured software development kit for JAVA.
+It includes everything we need to develop JAVA programs.
+This includes compilers, debuggers as well as tools to create programs.
+The JDK also contains the JRE which has the JVM(allows java to run on any platform -> Mac, window etc.)
+
+
+
+What is JShell?
+JShell is introduced in JAVA 9, and it still to continue in higher versions.
+JShell lives in the terminal or similar shell environment
+allows to see the result of code immediately
+it is great for testing the code.
+
+to open the jshell in cmd, just open the cmd and hit enter after typing jshell.
+/help is used to learn more about the things.
+ */
+public class A001_JAVA {
+}
