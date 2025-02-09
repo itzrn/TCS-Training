ec3d379755253be94f51b90d76c81ab8f8740b50
Aryan <aryangithub2017@gmail.com>
2025-02-09 16:19:05 +0530

    Push
    Push

--- OOPs/src/A001_JAVA.java
+++ OOPs/src/A001_JAVA.java
@@ -19,17 +19,6 @@ The JDK is a fully featured software development kit for JAVA.
 It includes everything we need to develop JAVA programs.
 This includes compilers, debuggers as well as tools to create programs.
 The JDK also contains the JRE which has the JVM(allows java to run on any platform -> Mac, window etc.)
-
-
-
-What is JShell?
-JShell is introduced in JAVA 9, and it still to continue in higher versions.
-JShell lives in the terminal or similar shell environment
-allows to see the result of code immediately
-it is great for testing the code.
-
-to open the jshell in cmd, just open the cmd and hit enter after typing jshell.
-/help is used to learn more about the things.
  */
 public class A001_JAVA {
 }
