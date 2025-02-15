f0167c2d3efe013c3994c5cfccdf14fa764b9cfc
Aryan <aryangithub2017@gmail.com>
2025-02-15 12:56:20 +0530

    Commit
    An Update To repo

--- OOPs/src/A017_Dog.java
+++
@@ -1,5 +0,0 @@
-public class A017_Dog implements A017_Pet{
-    public void play() {
-        System.out.println("Dog is playing");
-    }
-}
--- OOPs/src/A018_Answerable.java
+++
@@ -1,3 +0,0 @@
-public interface A018_Answerable {
-    String answer();
-}
