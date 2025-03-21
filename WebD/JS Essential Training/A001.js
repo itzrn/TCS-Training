6c86c39b36c25a09a9d1b5579b5a309e957d3f47
Aryan <aryangithub2017@gmail.com>
2025-03-21 21:02:11 +0530

    update
    Commit

---
+++ WebD/JS Essential Training/A001.js
@@ -0,0 +1,17 @@
+/*
+This language was initially called Mocha, then renamed to livescript and finally to
+Javascript
+
+Browsers use ECMAScript to interpret JS
+
+ECMAScript 2015 -> ES6
+ECMAScript 2016 -> ES7
+
+script tag run according to the flow of compiler
+so when need any js file, just call that js file after it
+
+mostly we write the script after the body tag
+*/
+
+let date = new Date();
+document.body.innerHTML = `<h1> Todays date is ${date}`;
---
+++ WebD/JS Essential Training/A002.js
@@ -0,0 +1,4 @@
+// The defined variable always have undefined value, untill its not initialized
+
+// typeof function is used to get the type of varibale
+
